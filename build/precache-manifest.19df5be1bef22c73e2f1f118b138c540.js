924823292de5297a236d881a3974f391c0f1bfde
ButkovskiyLev <levbut@gmail.com>
2020-09-14 23:53:05 +0500

    Initial

---
+++ build/precache-manifest.19df5be1bef22c73e2f1f118b138c540.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "899ba3910e8b713b640d9922b8c897a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3277ea5618d870f806bc",
+    "url": "/static/css/2.09e62520.chunk.css"
+  },
+  {
+    "revision": "3277ea5618d870f806bc",
+    "url": "/static/js/2.82a9c8b1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.82a9c8b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edafbb6f5d18d815aa94",
+    "url": "/static/js/main.f3811782.chunk.js"
+  },
+  {
+    "revision": "2f729f8dfb644b1198eb",
+    "url": "/static/js/runtime-main.4146942f.js"
+  }
+]);
